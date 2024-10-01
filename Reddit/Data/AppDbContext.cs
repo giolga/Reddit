3c56d8602ad0d194e5ccb01c236105015952a507
giolga <giolga.kumi@gmail.com>
2024-10-01 20:13:45 +0400

    OnModelCreatingConfig added

--- Reddit/Data/AppDbContext.cs
+++ Reddit/Data/AppDbContext.cs
@@ -7,7 +7,52 @@ namespace Reddit.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        public DbSet<Comment> Comments{ get; set; }
-        public DbSet<Post> Posts{ get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuring User and Post relationship (One-to-Many)
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.Author)
+                .WithMany(p => p.Posts)
+                .HasForeignKey(p => p.AuthorId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Post>()
+                .HasMany(p => p.Comments)
+                .WithOne(p => p.Post)
+                .HasForeignKey(p => p.PostId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Configuring User and Comment relationship (One-to-Many)
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.Author)
+                .WithMany(c => c.Comments)
+                .HasForeignKey(c => c.AuthorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Configuring Post and Comment relationship (One-to-Many)
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.Post)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(c => c.PostId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Posts)
+                .WithOne(u => u.Author)
+                .HasForeignKey(u => u.AuthorId);
+            //.OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Comments)
+                .WithOne(u => u.Author)
+                .HasForeignKey(u => u.AuthorId);
+            //.OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- Reddit/Model/User.cs
+++ Reddit/Model/User.cs
@@ -7,8 +7,10 @@ namespace Reddit.Model
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
         [Required]
+        [MaxLength(150)]
         public string Email { get; set; }
 
         public virtual List<Post> Posts { get; set; } = new List<Post>();
